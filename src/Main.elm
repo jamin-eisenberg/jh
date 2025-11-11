d035189c0fb1ae3ac25b3f0df0d15594a5c45d6a
Jamin Eisenberg <109154023+jeisenberg-chwy@users.noreply.github.com>
2025-11-11 15:12:02 -0500

    boilerplate

---
+++ src/Main.elm
@@ -0,0 +1,39 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html)
+
+
+type alias Model =
+    {}
+
+
+type Msg
+    = NoOp
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init flags =
+    ( {}, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    Html.text "Hi"
