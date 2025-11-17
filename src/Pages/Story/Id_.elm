e829ce40e6ebaefb362499211644ee2ff8b7d035
Jamin Eisenberg <109154023+jeisenberg-chwy@users.noreply.github.com>
2025-11-17 15:56:30 -0500

    Story page displays

--- src/Pages/Story/Id_.elm
+++ src/Pages/Story/Id_.elm
@@ -1,6 +1,9 @@
 module Pages.Story.Id_ exposing (page)
 
 import Gen.Params.Story.Id_ exposing (Params)
+import Gen.Route
+import Html exposing (a, button, div, h1, hr, img, p, text)
+import Html.Attributes exposing (class, href, src, style, type_)
 import Page exposing (Page)
 import Request
 import Shared
@@ -23,4 +26,16 @@ view maybeStory =
             { title = "Story not found", body = [] }
 
         Just story ->
-            { title = "JH - " ++ story.title, body = [] }
+            { title = "JH - " ++ story.title
+            , body =
+                [ div [ class "fixed-top" ]
+                    [ div [ class "d-flex flex-row align-items-center w-100 p-2" ]
+                        [ img [ src ("/images/" ++ story.iconImageName), style "height" "2.5em", style "margin-right" "1em" ] []
+                        , p [ class "fs-1 m-0" ] [ text story.title ]
+                        , a [ class "btn-close ms-auto", href (Gen.Route.toHref Gen.Route.Home_) ] []
+                        ]
+                    , hr [ class "mx-2 mt-0 mb-2" ] []
+                    ]
+                , p [ class "m-2", style "padding-top" "68px" ] [ text story.description ]
+                ]
+            }
